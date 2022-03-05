ea73eb55ab65f12500ac252777b3f269dce1bb6b
Recepguc <recep.gucc@gmail.com>
2022-03-05 23:53:08 +0300

    event listener added

--- App.js
+++ App.js
@@ -8,3 +8,18 @@ window.addEventListener("load",()=>{
     sessionStorage.setItem("taxRate", taxRate);
     sessionStorage.setItem("shippingPrice", shippingPrice);
 })
+let productsDiv=document.querySelector(".products");
+productsDiv.addEventListener("click",()=>{
+    let quantityP=event.target.parentElement.parentElement.querySelector("#product-quantity");
+    console.log(quantityP);
+    if(event.target.classList.contains("fa-minus")||event.target==quantityP.parentElement.firstChildElement){ 
+        console.log("minusbuton clicked");
+    }
+
+    else if(event.target.classList.contains("fa-plus")||event.target==quantityP.parentElement.lastChildElement){
+        console.log("plus button clicked");
+    }
+    else if(event.target.classList.contains("remove-product")||event.target==quantityP.parentElement.firstChildElement){
+        console.log("remove button clicked");
+    }
+})
